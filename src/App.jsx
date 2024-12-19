443c0f938a8132c606dcedae55799160503db8ed
Tim Charteris <tim.charterii@gmail.com>
2024-12-19 14:24:59 +0000

    update useEffect in App to utilise search but nothing works need to fix state and figure out handleSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setPosts, setLoading, setError } from './features/postsSlice'; // Import the action creators from the postsSlice
-import SearchBar from './components/searchBar';
+import SearchBar from './components/searchBar.jsx';
 
 
 export default function App() {
   const posts = useSelector((state) => state.posts.posts); // Select the posts array from the Redux store
   const loading = useSelector((state) => state.posts.loading); // Select the loading state from the Redux store
   const hasError = useSelector((state) => state.posts.hasError); // Select the hasError state from the Redux store
+  const searchTerm = useSelector((state) => state.posts.searchTerm); // Select the searchTerm state from the Redux store
 
   const dispatch = useDispatch(); // Create a dispatch function to send actions to store
 
@@ -21,13 +22,22 @@ export default function App() {
       dispatch(setLoading(true)); // setLoading to true in store
 
       try {
-        const response = await fetch('https://www.reddit.com/r/popular.json');
+        let url = '';
+        if (searchTerm.trim() === '') {
+          url = 'https://www.reddit.com/r/popular.json';
+        }
+        else {
+          url = `https://www.reddit.com/search.json?q=${searchTerm}`;
+        }
+
+        const response = await fetch(url);
         const data = await response.json();
 
         const redditPosts = data.data.children.map((post) => ({ // .map to create a new array of objects containing the relevant data
           id: post.data.id,
           title: post.data.title,
-          content: post.data.selftext
+          content: post.data.selftext,
+          image: post.data.thumbnail
         }));
 
         dispatch(setPosts(redditPosts)); // Dispatched the posts array just created to store
@@ -41,9 +51,11 @@ export default function App() {
       }
     };
     fetchPosts(); // Call the fetchPosts function
-  }, [dispatch]);
+  }, [dispatch, searchQuery]); // Add dispatch and searchQuery to the dependency array so it updates on load and on search
 
-
+  const handleSearch = (searchTerm) => {
+    // FIGURE OUT
+  }
 
 
   if (loading) {

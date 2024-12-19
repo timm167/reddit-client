30edf291c6a2397c109db4033a0827dab53f7859
Tim Charteris <tim.charterii@gmail.com>
2024-12-19 11:38:31 +0000

    create slice and receive in store

---
+++ src/features/postsSlice.js
@@ -0,0 +1,70 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+// Define the initial state to be used in postsSlice
+const initialState = [
+    {id:1, title: 'Post 1', content: 'This is Post 1 content'},
+    {id:2, title: 'Post 2', content: 'This is Post 2 content'}
+]
+
+
+// Create a slice for posts containing the name, initial state, and the reducers
+const postsSlice = createSlice({
+    name: 'posts', 
+    initialState, 
+    reducers: {
+        setPosts: (state, action) => {
+            return action.payload;  // Sets the posts array to the payload passed in the action
+            },
+        addPost: (state, action) => {
+            state.push(action.payload);  // state.push mutably adds a new post to the posts array thanks to toolkit
+            },
+    } 
+})
+
+// Export the action creators to be used in the components
+export const { setPosts, addPost } = postsSlice.actions;
+
+// Export the reducer to be used in the Redux store
+export default postsSlice.reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/store.js
+++ src/store.js
@@ -1,1 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+import postsReducer from './features/postsSlice.js'; 
+
+export const store = configureStore({
+    reducer: {
+        posts: postsReducer
+    }
+})

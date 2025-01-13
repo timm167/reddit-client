3e183f7ffa0fbee8b29b346922f1ec0db26f54fe
Tim Charteris <tim.charterii@gmail.com>
2025-01-13 18:18:52 +0000

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this matches the directory you're deploying
+    assetsDir: 'assets'
+  },
+  base: '/reddit-client/' // Set the base path if your repo name is reddit-client
 })
